b539d4e43ecd4fa0ec2bc5757e00a4e149f73c84
AFIFIHishame-Z <hishame.afifi1997@gmail.com>
2022-08-08 15:55:44 +0100

    Test

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -206,18 +206,12 @@ export default function Footer() {
                     </div>
                     {/*footer*/}
                     <div className="flex items-center justify-center p-6 border-t border-solid border-slate-200 rounded-b space-x-2">
-                      <FacebookShareButton
-                        url={imageUrl}
-                        openShareDialogOnClick={imageUrl !== ""}
-                      >
+                      <FacebookShareButton url={imageUrl}>
                         <FaFacebookSquare className="text-5xl text-blue-600 hover:text-blue-700 cursor-pointer" />
                       </FacebookShareButton>
                       <FaInstagramSquare className="text-5xl text-red-600 hover:text-red-700 cursor-pointer" />
 
-                      <TwitterShareButton
-                        url={imageUrl}
-                        openShareDialogOnClick={imageUrl !== ""}
-                      >
+                      <TwitterShareButton url={imageUrl}>
                         <FaTwitterSquare className="text-5xl text-blue-500 hover:text-blue-600 cursor-pointer" />
                       </TwitterShareButton>
                     </div>
